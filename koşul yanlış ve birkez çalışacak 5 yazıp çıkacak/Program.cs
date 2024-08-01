1e723194795a62be6f1aa3d486a6b753c6192146
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 16:06:18 +0300

    Add project files.

---
+++ koşul yanlış ve birkez çalışacak 5 yazıp çıkacak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koşul_yanlış_ve_birkez_çalışacak_5_yazıp_çıkacak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 4);
+            Console.ReadLine();
+        }
+    }
+}
